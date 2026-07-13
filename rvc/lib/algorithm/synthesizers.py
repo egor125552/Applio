619a6d757238184a798a161c2653b1db52a864d6
egor125552 <sitkosergej55@gmail.com>
2026-07-13 20:19:28 +0300

    Route synthesizer generators through registry

--- rvc/lib/algorithm/synthesizers.py
+++ rvc/lib/algorithm/synthesizers.py
@@ -1,41 +1,15 @@
-import torch
 from typing import Optional
-from rvc.lib.algorithm.generators.hifigan_mrf import HiFiGANMRFGenerator
-from rvc.lib.algorithm.generators.hifigan_nsf import HiFiGANNSFGenerator
-from rvc.lib.algorithm.generators.hifigan import HiFiGANGenerator
-from rvc.lib.algorithm.generators.refinegan import RefineGANGenerator
-from rvc.lib.algorithm.commons import slice_segments, rand_slice_segments
+
+import torch
+
+from rvc.lib.algorithm.commons import rand_slice_segments, slice_segments
+from rvc.lib.algorithm.encoders import PosteriorEncoder, TextEncoder
+from rvc.lib.algorithm.generators.registry import build_generator
 from rvc.lib.algorithm.residuals import ResidualCouplingBlock
-from rvc.lib.algorithm.encoders import TextEncoder, PosteriorEncoder
 
 
 class Synthesizer(torch.nn.Module):
-    """
-    Base Synthesizer model.
-
-    Args:
-        spec_channels (int): Number of channels in the spectrogram.
-        segment_size (int): Size of the audio segment.
-        inter_channels (int): Number of channels in the intermediate layers.
-        hidden_channels (int): Number of channels in the hidden layers.
-        filter_channels (int): Number of channels in the filter layers.
-        n_heads (int): Number of attention heads.
-        n_layers (int): Number of layers in the encoder.
-        kernel_size (int): Size of the convolution kernel.
-        p_dropout (float): Dropout probability.
-        resblock (str): Type of residual block.
-        resblock_kernel_sizes (list): Kernel sizes for the residual blocks.
-        resblock_dilation_sizes (list): Dilation sizes for the residual blocks.
-        upsample_rates (list): Upsampling rates for the decoder.
-        upsample_initial_channel (int): Number of channels in the initial upsampling layer.
-        upsample_kernel_sizes (list): Kernel sizes for the upsampling layers.
-        spk_embed_dim (int): Dimension of the speaker embedding.
-        gin_channels (int): Number of channels in the global conditioning vector.
-        sr (int): Sampling rate of the audio.
-        use_f0 (bool): Whether to use F0 information.
-        text_enc_hidden_dim (int): Hidden dimension for the text encoder.
-        kwargs: Additional keyword arguments.
-    """
+    """Base RVC synthesizer with generator selection delegated to a registry."""
 
     def __init__(
         self,
@@ -80,59 +54,22 @@ class Synthesizer(torch.nn.Module):
             text_enc_hidden_dim,
             f0=use_f0,
         )
+
         print(f"Using {vocoder} vocoder")
-        if use_f0:
-            if vocoder == "MRF HiFi-GAN":
-                self.dec = HiFiGANMRFGenerator(
-                    in_channel=inter_channels,
-                    upsample_initial_channel=upsample_initial_channel,
-                    upsample_rates=upsample_rates,
-                    upsample_kernel_sizes=upsample_kernel_sizes,
-                    resblock_kernel_sizes=resblock_kernel_sizes,
-                    resblock_dilations=resblock_dilation_sizes,
-                    gin_channels=gin_channels,
-                    sample_rate=sr,
-                    harmonic_num=8,
-                    checkpointing=checkpointing,
-                )
-            elif vocoder == "RefineGAN":
-                self.dec = RefineGANGenerator(
-                    sample_rate=sr,
-                    downsample_rates=upsample_rates[::-1],
-                    upsample_rates=upsample_rates,
-                    start_channels=16,
-                    num_mels=inter_channels,
-                    checkpointing=checkpointing,
-                )
-            else:
-                self.dec = HiFiGANNSFGenerator(
-                    inter_channels,
-                    resblock_kernel_sizes,
-                    resblock_dilation_sizes,
-                    upsample_rates,
-                    upsample_initial_channel,
-                    upsample_kernel_sizes,
-                    gin_channels=gin_channels,
-                    sr=sr,
-                    checkpointing=checkpointing,
-                )
-        else:
-            if vocoder == "MRF HiFi-GAN":
-                print("MRF HiFi-GAN does not support training without pitch guidance.")
-                self.dec = None
-            elif vocoder == "RefineGAN":
-                print("RefineGAN does not support training without pitch guidance.")
-                self.dec = None
-            else:
-                self.dec = HiFiGANGenerator(
-                    inter_channels,
-                    resblock_kernel_sizes,
-                    resblock_dilation_sizes,
-                    upsample_rates,
-                    upsample_initial_channel,
-                    upsample_kernel_sizes,
-                    gin_channels=gin_channels,
-                )
+        self.dec = build_generator(
+            vocoder,
+            use_f0=use_f0,
+            initial_channel=inter_channels,
+            resblock_kernel_sizes=resblock_kernel_sizes,
+            resblock_dilation_sizes=resblock_dilation_sizes,
+            upsample_rates=upsample_rates,
+            upsample_initial_channel=upsample_initial_channel,
+            upsample_kernel_sizes=upsample_kernel_sizes,
+            gin_channels=gin_channels,
+            sample_rate=sr,
+            checkpointing=checkpointing,
+        )
+
         self.enc_q = PosteriorEncoder(
             spec_channels,
             inter_channels,
@@ -181,7 +118,6 @@ class Synthesizer(torch.nn.Module):
         if y is not None:
             z, m_q, logs_q, y_mask = self.enc_q(y, y_lengths, g=g)
             z_p = self.flow(z, y_mask, g=g)
-            # regular old training method using random slices
             if self.randomized:
                 z_slice, ids_slice = rand_slice_segments(
                     z, y_lengths, self.segment_size
@@ -191,16 +127,29 @@ class Synthesizer(torch.nn.Module):
                     o = self.dec(z_slice, pitchf, g=g)
                 else:
                     o = self.dec(z_slice, g=g)
-                return o, ids_slice, x_mask, y_mask, (z, z_p, m_p, logs_p, m_q, logs_q)
-            # future use for finetuning using the entire dataset each pass
+                return o, ids_slice, x_mask, y_mask, (
+                    z,
+                    z_p,
+                    m_p,
+                    logs_p,
+                    m_q,
+                    logs_q,
+                )
+
+            if self.use_f0:
+                o = self.dec(z, pitchf, g=g)
             else:
-                if self.use_f0:
-                    o = self.dec(z, pitchf, g=g)
-                else:
-                    o = self.dec(z, g=g)
-                return o, None, x_mask, y_mask, (z, z_p, m_p, logs_p, m_q, logs_q)
-        else:
-            return None, None, x_mask, None, (None, None, m_p, logs_p, None, None)
+                o = self.dec(z, g=g)
+            return o, None, x_mask, y_mask, (
+                z,
+                z_p,
+                m_p,
+                logs_p,
+                m_q,
+                logs_q,
+            )
+
+        return None, None, x_mask, None, (None, None, m_p, logs_p, None, None)
 
     @torch.jit.export
     def infer(
@@ -212,17 +161,6 @@ class Synthesizer(torch.nn.Module):
         sid: torch.Tensor = None,
         rate: Optional[torch.Tensor] = None,
     ):
-        """
-        Inference of the model.
-
-        Args:
-            phone (torch.Tensor): Phoneme sequence.
-            phone_lengths (torch.Tensor): Lengths of the phoneme sequences.
-            pitch (torch.Tensor, optional): Pitch sequence.
-            nsff0 (torch.Tensor, optional): Fine-grained pitch sequence.
-            sid (torch.Tensor): Speaker embedding.
-            rate (torch.Tensor, optional): Rate for time-stretching.
-        """
         g = self.emb_g(sid).unsqueeze(-1)
         m_p, logs_p, x_mask = self.enc_p(phone, pitch, phone_lengths)
         z_p = (m_p + torch.exp(logs_p) * torch.randn_like(m_p) * 0.66666) * x_mask
@@ -239,5 +177,4 @@ class Synthesizer(torch.nn.Module):
             if self.use_f0
             else self.dec(z * x_mask, g=g)
         )
-
         return o, x_mask, (z, z_p, m_p, logs_p)
